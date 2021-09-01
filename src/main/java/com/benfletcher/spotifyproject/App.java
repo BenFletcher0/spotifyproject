4313ef00dc222f67292bbc6a5c30c7275e4dbfaa
benfletcher0 <benfletcher@wustl.edu>
2021-08-31 21:24:17 -0500

    add files

---
+++ src/main/java/com/benfletcher/spotifyproject/App.java
@@ -0,0 +1,13 @@
+package com.benfletcher.spotifyproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
