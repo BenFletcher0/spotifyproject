caca948c3749875c9de7b1bc8c2655dcf6771e8d
benfletcher0 <benfletcher@wustl.edu>
2021-09-04 17:26:15 -0500

    hid API key

--- src/main/java/com/benfletcher/spotifyproject/App.java
+++ src/main/java/com/benfletcher/spotifyproject/App.java
@@ -9,17 +9,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class App
-{
-    public static void main( String[] args ) throws IOException, InterruptedException {
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URL))
+                .uri(
+                        URI.create(
+                                "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=" +
+                                        API.API_KEY +
+                                        "&artist=Cher&album=Believe&format=json"
+                        )
+                )
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String responseString = response.body();
         Gson gson = new Gson();
-        Outer outer= gson.fromJson(responseString, Outer.class);
+        Outer outer = gson.fromJson(responseString, Outer.class);
         System.out.println(outer);
 
 
